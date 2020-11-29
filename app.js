b15b9206b57c710c8c4a19973a11ad9b7e52788f
rmzn <rmznkapar@gmail.com>
2020-11-29 22:57:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(permissions);
 app.use(lang);
 
 
-const Account = require('./models/account');
+const Account = require('./models/Account');
 passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
