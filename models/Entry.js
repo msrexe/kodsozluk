d106f715a817be27d6821d6bfefd9b4a78a0be00
rmznkapar <rmznkapar@gmail.com>
2020-12-02 02:32:56 +0300

    Update Entry.js

--- models/Entry.js
+++ models/Entry.js
@@ -4,15 +4,20 @@ var Schema = mongoose.Schema;
 var Entry = new Schema({
   title: {
     type: String,
-    required: true
+    required: true,
+    min: 3,
+    max: 24
   },
   body: {
     type: String,
-    required: true
+    required: true,
+    min: 3,
+    max: 200
   },
   user: {
     type: String,
-    required: true
+    required: true,
+    max: 16
   },
   date: {
     type: Date,
